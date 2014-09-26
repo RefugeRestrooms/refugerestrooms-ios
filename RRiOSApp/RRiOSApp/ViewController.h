b8b6af136ce7db3074ad45f906ceb24847d129cd
Harlan Kellaway <hkellaway.tech@gmail.com>
2014-09-26 12:58:32 -0700

    Sets up new Xcode project  and Travis

---
+++ RRiOSApp/RRiOSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RRiOSApp
+//
+//  Created by Harlan Kellaway on 9/26/14.
+//  Copyright (c) 2014 ___REFUGERESTROOMS___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RRiOSApp/RRiOSApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RRiOSApp
+//
+//  Created by Harlan Kellaway on 9/26/14.
+//  Copyright (c) 2014 ___REFUGERESTROOMS___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
