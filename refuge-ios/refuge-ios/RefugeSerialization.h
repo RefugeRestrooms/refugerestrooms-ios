3b08d5dcc3451d060786d6001cba29ce5e656035
Harlan Kellaway <hkellaway.tech@gmail.com>
2015-02-04 23:31:03 -0800

    Creates RefugeSerialization class

---
+++ refuge-ios/refuge-ios/RefugeSerialization.h
@@ -0,0 +1,20 @@
+//
+//  RefugeSerialization.h
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/4/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, RefugeSerializationErrorCode)
+{
+    RefugeSerializationErrorDeserializationFromJSONCode
+};
+
+@interface RefugeSerialization : NSObject
+
++ (NSArray *)deserializeRestroomsFromJSON:(NSArray *)JSON error:(NSError **)error;
+
+@end
---
+++ refuge-ios/refuge-ios/RefugeSerialization.m
@@ -0,0 +1,70 @@
+//
+//  RefugeSerialization.m
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/4/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import "RefugeSerialization.h"
+
+#import "MTLJSONAdapter.h"
+#import "RefugeRestroom.h"
+
+NSString *RefugeSerializationErrorDomain = @"RefugeSerializationErrorDomain";
+
+@implementation RefugeSerialization
+
++ (NSArray *)deserializeRestroomsFromJSON:(NSArray *)JSON error:(NSError **)error
+{
+    if (!JSON || ![JSON isKindOfClass:[NSArray class]] || [JSON count] == 0)
+    {
+        return nil;
+    }
+    
+    NSError *errorWhileDeserializingJSON;
+    NSMutableArray *restrooms = [NSMutableArray new];
+    
+    for (NSDictionary *restroomJSON in JSON)
+    {
+        RefugeRestroom *restroom = [RefugeSerialization deserializeRestroomFromJSON:restroomJSON error:&errorWhileDeserializingJSON];
+        
+        if(errorWhileDeserializingJSON)
+        {
+            NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithCapacity:1];
+            
+            [userInfo setObject:errorWhileDeserializingJSON forKey:NSUnderlyingErrorKey];
+            
+            *error = [NSError errorWithDomain:RefugeSerializationErrorDomain code:RefugeSerializationErrorDeserializationFromJSONCode userInfo:userInfo];
+        }
+        
+        if (restroom)
+        {
+            [restrooms addObject:restroom];
+        }
+    }
+    
+    return [restrooms count] > 0 ? [NSArray arrayWithArray:restrooms] : nil;
+}
+
+# pragma mark - Helper methods
+
++ (RefugeRestroom *)deserializeRestroomFromJSON:(NSDictionary *)JSON error:(NSError **)error
+{
+    NSError *errorWhileDeserializingJSON;
+    
+    RefugeRestroom *restroom = [MTLJSONAdapter modelOfClass:[RefugeRestroom class] fromJSONDictionary:JSON error:&errorWhileDeserializingJSON];
+    
+    if (errorWhileDeserializingJSON)
+    {
+        NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithCapacity:1];
+        
+        [userInfo setObject:errorWhileDeserializingJSON forKey:NSUnderlyingErrorKey];
+        
+        *error = [NSError errorWithDomain:RefugeSerializationErrorDomain code:RefugeSerializationErrorDeserializationFromJSONCode userInfo:userInfo];
+    }
+    
+    return restroom;
+}
+
+@end
