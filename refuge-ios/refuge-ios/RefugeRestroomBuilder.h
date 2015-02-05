96ac6180c400862185b7117ad513398b62f3bee2
Harlan Kellaway <hkellaway.tech@gmail.com>
2015-02-04 21:15:32 -0800

    Creates RestroomBuilder class

---
+++ refuge-ios/refuge-ios/RefugeRestroomBuilder.h
@@ -0,0 +1,13 @@
+//
+//  RefugeRestroomBuilder.h
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/4/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RefugeRestroomBuilder : NSObject
+
+@end
---
+++ refuge-ios/refuge-ios/RefugeRestroomBuilder.m
@@ -0,0 +1,13 @@
+//
+//  RefugeRestroomBuilder.m
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/4/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import "RefugeRestroomBuilder.h"
+
+@implementation RefugeRestroomBuilder
+
+@end
