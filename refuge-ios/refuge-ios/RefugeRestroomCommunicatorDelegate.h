2a7f59dca084af0c5cf456fea306f1de17fc73c1
Harlan Kellaway <hkellaway.tech@gmail.com>
2015-02-05 09:55:24 -0800

    Creates RestroomCommunicatorDelegate

---
+++ refuge-ios/refuge-ios/RefugeRestroomCommunicatorDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RefugeRestroomCommunicatorDelegate.h
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/5/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol RefugeRestroomCommunicatorDelegate <NSObject>
+
+@end
