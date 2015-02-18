0f786776d2c6086ddd85a2223530e9bbd65acac0
H Kellaway <hkellaway.tech@gmail.com>
2015-02-17 17:29:41 -0800

    Implements web view in New Restroom controller

---
+++ refuge-ios/refuge-ios/RefugeNewRestroomViewController.h
@@ -0,0 +1,13 @@
+//
+//  RefugeNewRestroomViewController.h
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/17/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RefugeNewRestroomViewController : UIViewController
+
+@end
