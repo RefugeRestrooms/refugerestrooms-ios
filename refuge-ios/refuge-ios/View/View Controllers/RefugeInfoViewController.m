6028b9b46cdee2a6f12eb8414d2660df5166f7bd
H Kellaway <hkellaway.tech@gmail.com>
2015-02-21 18:32:46 -0800

    Adds gesture recognizers to social media images

--- refuge-ios/refuge-ios/View/View Controllers/RefugeInfoViewController.m
+++ refuge-ios/refuge-ios/View/View Controllers/RefugeInfoViewController.m
@@ -10,13 +10,52 @@
 
 @interface RefugeInfoViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *githubImage;
+@property (weak, nonatomic) IBOutlet UIImageView *facebookImage;
+@property (weak, nonatomic) IBOutlet UIImageView *twitterImage;
+
 @end
 
 @implementation RefugeInfoViewController
 
+#pragma mark - View life-cycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self addGestureRecognizersToImages];
+}
+
+#pragma mark - Touch
+
+- (void)didTouchGithubImage
+{
+    NSLog(@"Github");
+}
+
+- (void)didTouchFacebookImage
+{
+    NSLog(@"Facebook");
+}
+
+- (void)didTouchTwitterImage
+{
+    NSLog(@"Twitter");
+}
+
+#pragma mark - Private methods
+
+- (void)addGestureRecognizersToImages
+{
+    UITapGestureRecognizer *githubGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTouchGithubImage)];
+    [self.githubImage addGestureRecognizer:githubGestureRecognizer];
+    
+    UITapGestureRecognizer *facebookGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTouchFacebookImage)];
+    [self.facebookImage addGestureRecognizer:facebookGestureRecognizer];
+    
+    UITapGestureRecognizer *twitterGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTouchTwitterImage)];
+    [self.twitterImage addGestureRecognizer:twitterGestureRecognizer];
 }
 
 @end
