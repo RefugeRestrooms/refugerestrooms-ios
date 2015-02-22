a46385072d2c6ec0aae4635b2ac3288a7424e989
H Kellaway <hkellaway.tech@gmail.com>
2015-02-21 18:13:14 -0800

    Adds social media images to info view

---
+++ refuge-ios/refuge-ios/View/View Controllers/RefugeInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  RefugeInfoViewController.h
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/21/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RefugeInfoViewController : UIViewController
+
+@end
---
+++ refuge-ios/refuge-ios/View/View Controllers/RefugeInfoViewController.m
@@ -0,0 +1,22 @@
+//
+//  RefugeInfoViewController.m
+//  refuge-ios
+//
+//  Created by Harlan Kellaway on 2/21/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import "RefugeInfoViewController.h"
+
+@interface RefugeInfoViewController ()
+
+@end
+
+@implementation RefugeInfoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
