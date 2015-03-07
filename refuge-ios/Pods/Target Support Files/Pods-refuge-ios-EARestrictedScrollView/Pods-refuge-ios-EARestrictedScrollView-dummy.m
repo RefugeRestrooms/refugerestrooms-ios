f2ada7e0af20f56a85406381d269f27849df403c
H Kellaway <hkellaway.tech@gmail.com>
2015-03-07 14:12:45 -0800

    Upgrades Crashlytics to Fabric per Issue #111

---
+++ refuge-ios/Pods/Target Support Files/Pods-refuge-ios-EARestrictedScrollView/Pods-refuge-ios-EARestrictedScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_refuge_ios_EARestrictedScrollView : NSObject
+@end
+@implementation PodsDummy_Pods_refuge_ios_EARestrictedScrollView
+@end
