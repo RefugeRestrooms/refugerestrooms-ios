b64a1123211cf6a6288230517fb346e0396beff5
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-30 13:20:31 -0700

    Replaces licensing text in RefugeDataPersistenceManager.m per issue #58

--- refuge-ios/refuge-ios/Logic/Data Persistence/RefugeDataPersistenceManager.m
+++ refuge-ios/refuge-ios/Logic/Data Persistence/RefugeDataPersistenceManager.m
@@ -1,10 +1,20 @@
 //
-//  RefugeDataPersistenceManager.m
-//  refuge-ios
+// RefugeDataPersistenceManager.m
 //
-//  Created by Harlan Kellaway on 2/7/15.
-//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+// Copyleft (c) 2015 Refuge Restrooms
 //
+// Refuge is licensed under the GNU AFFERO GENERAL PUBLIC LICENSE
+// Version 3, 19 November 2007
+//
+// This notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "RefugeDataPersistenceManager.h"
 
@@ -25,12 +35,12 @@
 - (id)init
 {
     self = [super init];
-    
+
     if(self)
     {
         self.managedObjectContext = ((RefugeAppDelegate *)[UIApplication sharedApplication].delegate).managedObjectContext;
     }
-    
+
     return self;
 }
 
@@ -39,24 +49,24 @@
 - (NSArray *)allRestrooms
 {
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[RefugeRestroom managedObjectEntityName]];
-    
+
     NSError *error = nil;
     NSArray *allRestrooms = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
-    
+
     if(error)
     {
         [self.delegate retrievingAllRestroomsFailedWithError:error];
-        
+
         return nil;
     }
-    
+
     return allRestrooms;
 }
 
 - (void)saveRestrooms:(NSArray *)restrooms
 {
     NSError *errorSavingRestrooms;
-    
+
     for(RefugeRestroom *restroom in restrooms)
     {
         [MTLManagedObjectAdapter managedObjectFromModel:restroom
@@ -64,7 +74,7 @@
                                                   error:&errorSavingRestrooms];
         [self.managedObjectContext save:&errorSavingRestrooms];
     }
-    
+
     if(errorSavingRestrooms)
     {
         [self.delegate savingRestroomsFailedWithError:errorSavingRestrooms];
