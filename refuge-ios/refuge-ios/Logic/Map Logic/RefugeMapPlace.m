030eb501977a83f55ed5b6031baad21691fbe4d8
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-30 13:07:30 -0700

    Replaces licensing text in RefugeMapPlace.m per issue #58

--- refuge-ios/refuge-ios/Logic/Map Logic/RefugeMapPlace.m
+++ refuge-ios/refuge-ios/Logic/Map Logic/RefugeMapPlace.m
@@ -1,10 +1,20 @@
 //
-//  RefugeMapPlace.m
-//  refuge-ios
+// RefugeMapPlace.m
 //
-//  Created by Harlan Kellaway on 2/10/15.
-//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+// Copyleft (c) 2015 Refuge Restrooms
 //
+// Refuge is licensed under the GNU AFFERO GENERAL PUBLIC LICENSE
+// Version 3, 19 November 2007
+//
+// This notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "RefugeMapPlace.h"
 
@@ -23,12 +33,12 @@
 - (instancetype)init
 {
     self = [super init];
-    
+
     if(self)
     {
         self.place = [[SPGooglePlacesAutocompletePlace alloc] init];
     }
-    
+
     return self;
 }
 
@@ -37,7 +47,7 @@
 - (void)resolveToPlacemarkWithSuccessBlock:(void (^)(CLPlacemark *))placemarkSuccess failure:(void (^)(NSError *))placemarkError
 {
     SPGooglePlacesAutocompletePlace *place = [self translateToSPPlace];
-    
+
     [place resolveToPlacemark:^(CLPlacemark *placemark, NSString *addressString, NSError *error) {
         if(error)
         {
@@ -48,7 +58,7 @@
             placemarkSuccess(placemark);
         }
     }];
-    
+
 }
 
 # pragma mark - Private methods
@@ -56,16 +66,16 @@
 - (SPGooglePlacesAutocompletePlace *)translateToSPPlace
 {
     NSString *typeString = [self typeToString];
-    
+
     NSDictionary *dictionary = @{
                                  @"description" : self.name,
                                  @"reference" : self.reference,
                                  @"id" : self.identifier,
                                  @"types" : [NSArray arrayWithObject:typeString]
                                  };
-    
+
     SPGooglePlacesAutocompletePlace *place = [SPGooglePlacesAutocompletePlace placeFromDictionary:dictionary apiKey:self.key];
-    
+
     return place;
 }
 
